41b74de000550a7d50f0ea9105aca74ded5d03c7
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-06 20:58:27 +0100

    builder: remove print statements
    needed them to peek under the hood of tokenstream

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,20 +1,11 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, __private::ToTokens};
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let a = input.attrs;
-    for x in a {
-        println!("{}", x.to_token_stream());
-    }
-    match input.data {
-        Data::Struct(ref s) => match s.fields {
-            Fields::Named(ref fields) => {
-                for f in fields.named.iter() {
-                    println!("{:?}{:?}", f.ident, f.to_token_stream());
+    let _input = parse_macro_input!(input as DeriveInput);
+
     let expanded = quote! {
         pub struct CommandBuilder {
             executable: Option<String>,
@@ -33,16 +24,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     current_dir: None,
                 }
             }
-            _ => println!("else"),
-        },
-        _ => println!("else"),
-    }
-    println!(
-        "{}{}{}",
-        input.vis.to_token_stream(),
-        input.generics.to_token_stream(),
-        input.ident.to_token_stream()
-    );
         }
 
     };
