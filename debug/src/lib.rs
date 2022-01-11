658c68956600a17bbc8da173c17edcec8c936e68
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-11 06:20:38 +0100

    debug: passing test 6 - refactor (finally:)

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(unused_variables, dead_code, unused_imports)]
 use std::any::Any;
 
-use proc_macro::{Span, TokenStream};
+use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use syn::Attribute;
 use syn::DeriveInput;
@@ -14,9 +14,10 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         input.ident.to_string().as_str(),
         proc_macro2::Span::call_site(),
     );
+    let b = input.fields.build_debug_struct_fields();
     if let Some(g) = input.generics.type_params().last() {
         let ident = &g.ident;
-        if let Some(q) = input.fields.contains_phantom_field() {
+        if let Some(q) = input.fields.get_phantom_field() {
             proc_macro::TokenStream::from(quote! {
                 impl<#ident> std::fmt::Debug for #i<#ident>
                 where
@@ -24,8 +25,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 {
                     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                         f.debug_struct(#u)
-                        .field("string", &self.string)
-                        .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
+                        #b
                         .finish()
                     }
                 }
@@ -35,8 +35,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 impl<#ident: std::fmt::Debug> std::fmt::Debug for #i<#ident> {
                     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                         f.debug_struct(#u)
-                        .field("value", &self.value)
-                        .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
+                        #b
                         .finish()
                     }
                 }
@@ -47,8 +46,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             impl std::fmt::Debug for #i {
                 fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                     f.debug_struct(#u)
-                    .field("name", &self.name)
-                    .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
+                    #b
                     .finish()
                 }
             }
@@ -67,12 +65,14 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 // }
 
 trait FieldsParser {
-    type PhantomGeneric;
-    fn contains_phantom_field(&self) -> std::option::Option<syn::Ident>;
+    // type PhantomGeneric;
+    fn get_phantom_field(&self) -> std::option::Option<syn::Ident>;
+    fn build_debug_struct_fields(&self) -> proc_macro2::TokenStream;
+    fn debug_field_format(f: &syn::Field) -> syn::Result<proc_macro2::TokenStream>;
 }
 impl FieldsParser for syn::Fields {
-    type PhantomGeneric = syn::AngleBracketedGenericArguments;
-    fn contains_phantom_field(&self) -> Option<syn::Ident> {
+    // type PhantomGeneric = syn::AngleBracketedGenericArguments;
+    fn get_phantom_field(&self) -> Option<syn::Ident> {
         for f in self.iter() {
             if let syn::Type::Path(syn::TypePath { qself, path }) = &f.ty {
                 if let Some(s) = path.segments.first() {
@@ -83,5 +83,34 @@ impl FieldsParser for syn::Fields {
             }
         }
         None
+    }
+    fn debug_field_format(f: &syn::Field) -> syn::Result<proc_macro2::TokenStream> {
+        let field_name = &f.ident;
+        if let Some(a) = f.attrs.first() {
+            if let Ok(syn::Meta::NameValue(syn::MetaNameValue { lit, path, .. })) = a.parse_meta() {
+                if path.is_ident("debug") {
+                    if let syn::Lit::Str(l) = lit {
+                        return Ok(quote! { &format_args!(#l, &self.#field_name)});
+                    }
+                } else {
+                    return Err(syn::Error::new_spanned(f, "only debug is implpemented"));
+                }
+            }
+        }
+        Ok(quote! { &self.#field_name })
+    }
+    fn build_debug_struct_fields(&self) -> proc_macro2::TokenStream {
+        let fields = self
+            .iter()
+            .map(|f| {
+                let a = syn::LitStr::new(
+                    f.ident.as_ref().unwrap().to_string().as_str(),
+                    proc_macro2::Span::mixed_site(),
+                );
+                let c = Self::debug_field_format(&f).unwrap();
+                return quote! { .field(#a, #c) };
+            })
+            .collect::<Vec<_>>();
+        quote! {#(#fields)*}.into()
     }
 }
