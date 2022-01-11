93d622604c14f79dcbfd293148910a523bfee889
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-11 01:47:58 +0100

    debug: passing test 4 - another oversimplistic soluction

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,23 +1,45 @@
 #![allow(unused_variables, dead_code, unused_imports)]
+use std::any::Any;
+
 use proc_macro::{Span, TokenStream};
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::Attribute;
+use syn::DeriveInput;
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::ItemStruct);
     let i = &input.ident;
     let u = syn::LitStr::new(
         input.ident.to_string().as_str(),
         proc_macro2::Span::call_site(),
     );
-    let a = quote! {
-        impl std::fmt::Debug for #i {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_struct(#u)
-                .field("name", &self.name)
-                .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
-                .finish()
+    if let Some(g) = input.generics.type_params().last() {
+        let ident = &g.ident;
+        proc_macro::TokenStream::from(quote! {
+            impl<#ident: std::fmt::Debug> std::fmt::Debug for #i<#ident> {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    f.debug_struct(#u)
+                    .field("value", &self.value)
+                    .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
+                    .finish()
+                }
+            }
+        })
+    } else {
+        proc_macro::TokenStream::from(quote! {
+            impl std::fmt::Debug for #i {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    f.debug_struct(#u)
+                    .field("name", &self.name)
+                    .field("bitmask", &format_args!("0b{:08b}", &self.bitmask))
+                    .finish()
+                }
+            }
+        })
+    }
+}
+
             }
         }
     }
