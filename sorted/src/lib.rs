93b1639b58ccd1f395810b49d023cbf32f1a95a8
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-14 15:12:06 +0100

    sorted: passing test 3

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -14,7 +14,40 @@ pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 fn sorted_variants(input: syn::Item) -> syn::Result<proc_macro2::TokenStream> {
-    if let syn::Item::Enum(_x) = input {
+    if let syn::Item::Enum(x) = input {
+        if let Some(is_sorted) = x
+            .variants
+            .iter()
+            .collect::<Vec<&syn::Variant>>()
+            .windows(2)
+            .map(|v| {
+                if v[0].ident.to_string() <= v[1].ident.to_string() {
+                    None
+                } else {
+                    Some(v[1])
+                }
+            })
+            .filter(|v| v.is_some())
+            .map(|v| v.unwrap())
+            .inspect(|x| println!("{:?}", x.ident.to_string()))
+            .collect::<Vec<&syn::Variant>>()
+            .first()
+        {
+            let before = x
+                .variants
+                .iter()
+                .find(|x| x.ident.to_string() >= is_sorted.ident.to_string())
+                .unwrap();
+            return Err(syn::Error::new(
+                is_sorted.ident.span(),
+                format!(
+                    "{} should sort before {}",
+                    is_sorted.ident.to_string(),
+                    before.ident.to_string()
+                ),
+            ));
+        }
+
         Ok(quote! {})
     } else {
         Err(syn::Error::new(
