bd3ae7ac07b0303b8656ad08be7f0fac29c463b2
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-14 14:24:26 +0100

    sorted: passing test 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,10 +1,25 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as syn::Item);
+    // let args = parse_macro_input!(args as syn::Item);
+    if let Err(e) = sorted_variants(input) {
+        TokenStream::from(e.to_compile_error())
+    } else {
+        quote! {}.into()
+    }
+}
 
-    quote! {}.into()
+fn sorted_variants(input: syn::Item) -> syn::Result<proc_macro2::TokenStream> {
+    if let syn::Item::Enum(_x) = input {
+        Ok(quote! {})
+    } else {
+        Err(syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        ))
+    }
 }
