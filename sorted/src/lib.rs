20cfaae083fc4d5799ea8ba520cccbd9c9ca2d07
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-14 15:19:14 +0100

    sorted: passing test 4

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -4,17 +4,18 @@ use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut out = input.clone();
     let input = parse_macro_input!(input as syn::Item);
     // let args = parse_macro_input!(args as syn::Item);
     if let Err(e) = sorted_variants(input) {
-        TokenStream::from(e.to_compile_error())
-    } else {
-        quote! {}.into()
+        out.extend(TokenStream::from(e.to_compile_error()));
     }
+    out
 }
 
 fn sorted_variants(input: syn::Item) -> syn::Result<proc_macro2::TokenStream> {
     if let syn::Item::Enum(x) = input {
+        // dbg!(&x);
         if let Some(is_sorted) = x
             .variants
             .iter()
