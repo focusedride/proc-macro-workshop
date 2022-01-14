83d666db8fb73e6158c2014809d507c6513c80cc
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-14 13:45:02 +0100

    sorted: passing test 1

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let _ = input;
 
-    unimplemented!()
+    quote! {}.into()
 }
