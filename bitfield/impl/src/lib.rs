64106ccedf1f99f737fa4ffc62130b274d95e2ec
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-01-15 05:13:31 +0100

    bitfield: passing test 2

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -11,11 +11,13 @@ pub fn bitfield(
     let _ = args;
     let input = parse_macro_input!(input as syn::ItemStruct);
     let types = input.build_bit_types();
-    quote! {#input #types}.into()
+    let data = input.build_struct_with_data_array();
+    quote! {#data #types}.into()
 }
 
 trait Bitfield {
     fn build_bit_types(&self) -> TokenStream;
+    fn build_struct_with_data_array(&self) -> TokenStream;
 }
 impl Bitfield for syn::ItemStruct {
     fn build_bit_types(&self) -> TokenStream {
@@ -25,12 +27,37 @@ impl Bitfield for syn::ItemStruct {
                 let type_ident = syn::Ident::new(format!("B{}", i).as_str(), span);
                 let value = syn::LitInt::new(&i.to_string(), span);
                 quote! {
-                pub enum #type_ident {}
-                impl bitfield::Specifier for #type_ident {
-                    const BITS: u8 = #value;
-                }}
+                    pub enum #type_ident {}
+                    impl bitfield::Specifier for #type_ident {
+                        const BITS: u8 = #value;
+                    }
+                }
             })
             .collect();
         quote! { #(#enums)* }
+    }
+    fn build_struct_with_data_array(&self) -> TokenStream {
+        let mut size = 0;
+        for f in &self.fields {
+            if let syn::Type::Path(syn::TypePath { path, .. }) = &f.ty {
+                size += path
+                    .segments
+                    .first()
+                    .unwrap()
+                    .ident
+                    .to_string()
+                    .trim_start_matches("B")
+                    .parse::<u16>()
+                    .unwrap();
+            }
+        }
+        let span = proc_macro2::Span::call_site();
+        let size = syn::LitInt::new(&(size / 8).to_string(), span);
+        quote! {
+            #[repr(C)]
+            pub struct MyFourBytes {
+                data: [u8; #size],
+            }
+        }
     }
 }
